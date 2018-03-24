6d123a565633a7046c34d0a0d87bb6eb4c905828
Souvannavong <souvannavong.thibault@hotmail.fr>
2018-03-25 00:26:30 +0100

    page résultat

--- siteweb/resultat.php
+++ siteweb/resultat.php
@@ -29,19 +29,74 @@
 	$bdd = new PDO('mysql:host=localhost:8889;dbname=movenmeet;charset=utf8',
 'root', 'root'); 
 ?><?php
-echo '<div class="activite">';
+
 if(isset($_GET["saisie"])){
 	$saisie=$_GET["saisie"];
 	$req = $bdd->query("select * from etablissement where Type LIKE '%$saisie%' ");
  	while($donne = $req->fetch()){
- 		echo $donne['Nom'];
+ 		echo '<div class="activite">';
+ 		echo "<h1>".$donne['Nom']."</h1>";
+ 		echo "<img src='images/boire.jpg'><br/>";
+ 		echo $donne['Numéro']." ".$donne['Rue']." <br/><br/>".$donne['Horaires']."<br/><br/>".$donne['Cuisine'];
+ 		echo '</div>';
+ 		}
+ 		
  		} 
- 	//if(($saisie == " ") || ($saisie =="%")) {
- 			//echo "Pas de résultats trouvés";
- 	//}
-echo '</div>';
+ ?>
+ <?php
+
+if(isset($_GET["saisie"])){
+	$saisie=$_GET["saisie"];
+	$req = $bdd->query("select * from exterieure where Type LIKE '%$saisie%' ");
+ 	while($donne = $req->fetch()){
+ 		echo '<div class="activite">';
+ 		echo "<h1>".$donne['Nom']."</h1>";
+ 		echo "<img src='images/rech.jpg'><br/>";
+		echo $donne['Ville']." <br/><br/>".$donne['Distance']."<br/><br/>".$donne['Classement']."<br/><br/>". $donne['Eau']."<br/><br/>".$donne['Description'];
+ 		echo '</div>';
+ 		} 
  	}
  ?>
+  <?php
+
+if(isset($_GET["saisie"])){
+	$saisie=$_GET["saisie"];
+	$req = $bdd->query("select * from culture where Type LIKE '%$saisie%' ");
+ 	while($donne = $req->fetch()){
+ 		echo '<div class="activite">';
+				echo "<h1>".$donne['Nom']."<br/></h1>";
+				echo "<img src='images/rech.jpg'>";
+				echo $donne['Numero_Rue']." ".$donne['Rue']." ".$donne['Ville']."<br/>";
+				echo "</div>";
+ 		} 
+ 	}
+ ?>
+ <?php
+
+if(isset($_GET["saisie"])){
+	$saisie=$_GET["saisie"];
+	$req = $bdd->query("select * from divers where Type LIKE '%$saisie%' ");
+ 	while($donne = $req->fetch()){
+ 		echo '<div class="activite">';
+				echo "<h1>".$donne['Nom']."<br/></h1>";
+				echo "<img src='images/rech.jpg'>";
+				echo "</div>";
+ 		} 
+ 	}
+ ?>
+  <?php
+
+if(isset($_GET["saisie"])){
+	$saisie=$_GET["saisie"];
+	$req = $bdd->query("select * from evenement where Type LIKE '%$saisie%' ");
+ 	while($donne = $req->fetch()){
+ 		echo '<div class="activite">';
+echo'<h1>'.$donne['Nom'].'</h1><br/><img src='.$donne['Photo']."><h2>Lieu :</h2>".$donne['Lieu']."<br/><h2>Date :</h2>".$donne['Date']."<br/><h2>Type :</h2>".$donne['Type'];
+echo '</div>';
+ 		} 
+ 	}
+ ?>
+ 
  
 </body>
 </html>

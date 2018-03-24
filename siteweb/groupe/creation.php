2ebf73e4c6c8c7598a80824c2fc56ef33c6680f3
Laetitia-V <laetitia.viau@hotmail.fr>
2018-03-24 13:26:49 +0100

    suppression de "BONS PLANS" sur toutes les pages

--- siteweb/groupe/creation.php
+++ siteweb/groupe/creation.php
@@ -30,7 +30,6 @@
 
 <p class="ongletsPageA">
 <span><a href="trouver_act.php"> TROUVER UNE ACTIVITÉ</a></span>
-<span><a href="bons_plans.html">BONS PLANS </a></span>
 <span><a href="groupe.php"> ACTIVITÉS DE GROUPE</a></span>
 <span><a href="evenements.php"> ÉVENEMENTS</a> </span>
 </p>

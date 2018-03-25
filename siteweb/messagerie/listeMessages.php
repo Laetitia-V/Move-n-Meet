1e0392045b43d5f8b27d8298e25a3138ba9c8ca3
BenMlahA <35455977+BenMlahA@users.noreply.github.com>
2018-03-25 21:19:34 +0200

    listeMessage

--- siteweb/messagerie/listeMessages.php
+++ siteweb/messagerie/listeMessages.php
@@ -8,6 +8,11 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
       <link rel="stylesheet" href="../styles/style.css" type="text/css" media="screen" />
+    <script	type="text/javascript">
+        function affiche_message(e){
+    var p=document.getElementById(e);
+    p.style = "display: block;" ;}
+    </script>
 <title>Move n' Meet</title>
 </head>
 
@@ -26,9 +31,11 @@
         $req= $bdd -> query ("SELECT * FROM utilisateur WHERE Id_utilisateur='".$idEnvoyeur."'");
         $env= $req -> fetch();
         $de= $env['Mail'];
-        echo '<div class="mess">';
-        echo '<p> <strong>DE</strong> :  '.$de.'    <strong>Objet</strong> : '.$mess['Objet'].'</p>';
+        echo '<div class="mess" onclick=affiche_message("messp")>';
+        echo '<p > <strong>DE</strong> :  '.$de.'    <strong>Objet</strong> : '.$mess['Objet'].'</p>';
+        echo '<div id="messp">';
         echo "<p>".$mess['Message']." <a class='reponse' href='envoiMessage.php?idReceveur=".$idEnvoyeur."'>Repondre</a></p>";
+        echo '</div>';
         echo '</div>';
    
     }
